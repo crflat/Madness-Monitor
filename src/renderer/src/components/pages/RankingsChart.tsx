86e6d964768d086d2ab69b5d9d0aeab2e77bac66
crflat <flatley.colin@gmail.com>
2026-09-02 10:24:01 -0400

    added and styled the navigation bar

---
+++ src/renderer/src/components/pages/RankingsChart.tsx
@@ -0,0 +1,16 @@
+import { CContainer } from '@coreui/react/dist/esm/components/grid/CContainer'
+
+function RankingsChart(): React.JSX.Element {
+  return (
+    <>
+      <CContainer
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ height: '100%' }}
+      >
+        <h1>Rankings Chart</h1>
+      </CContainer>
+    </>
+  )
+}
+
+export default RankingsChart
---
+++ src/renderer/src/components/pages/Scorecards.tsx
@@ -0,0 +1,16 @@
+import { CContainer } from '@coreui/react/dist/esm/components/grid/CContainer'
+
+function Scorecards(): React.JSX.Element {
+  return (
+    <>
+      <CContainer
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ height: '100%' }}
+      >
+        <h1>Scorecards</h1>
+      </CContainer>
+    </>
+  )
+}
+
+export default Scorecards
